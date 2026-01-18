cbd52bb0f87142d0129dadb502093234d9ed91ed
Roman <romabychkof@yandex.ru>
2026-01-18 12:30:30 +0300

    miniapp v1

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
+console.log('initData:', tg.initData);
+console.log('initDataUnsafe:', tg.initDataUnsafe);  
+
 state.user = tg.initDataUnsafe?.user || null;
 state.initData = tg.initData || null;
 
